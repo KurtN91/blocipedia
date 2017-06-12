2da8a7a3f1b75297e742c7afdf3c4a4374aa6e27
kevrok <kurt.neaderhiser@gmail.com>
2017-06-12 16:15:16 +0000

    attempted heroku workaround

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-            validates :name, length: { minimum: 1, maximum: 100 }, presence: true
- # #4
-  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-  validates :password, length: { minimum: 6 }, allow_blank: true
- # #5
-   validates :email,
-             presence: true,
-             uniqueness: { case_sensitive: false },
-             length: { minimum: 3, maximum: 254 }
-
-  before_save :format_name 
- # #6
-   has_secure_password
 end
