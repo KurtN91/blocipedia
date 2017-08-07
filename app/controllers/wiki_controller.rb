ddbe64395cd761c0fc547f6f83a96d06602e577e
kevrok <kurt.neaderhiser@gmail.com>
2017-08-07 17:29:58 +0000

    Checkpoint 5 done

---
+++ app/controllers/wiki_controller.rb
@@ -0,0 +1,62 @@
+class WikiController < ApplicationController
+  def index
+    @wikis = policy_scope(Wiki)
+  end
+
+  def show
+    @wiki = Wiki.find(params[:id])
+    authorize @wiki
+  end
+
+  def new
+    @wiki = Wiki.new
+    authorize @wiki
+  end
+
+  def create
+    @wiki = current_user.wikis.create(wiki_params)
+    authorize @wiki
+    if @wiki.save
+      flash[:notice] = "Wiki was saved."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error creating your wiki."
+      redirect_to :new
+    end
+  end
+
+  def edit
+    @wiki = Wiki.find(params[:id])
+    authorize @wiki
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    authorize @wiki
+    if @wiki.update_attributes(wiki_params)
+      flash[:notice] = "Wiki was successfully updated." 
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error editing the post. Please try again."
+      redirect_to :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+    authorize @wiki
+    if @wiki.destroy
+      flash[:notice] = "Successfully deleted your Wiki."
+      redirect_to wikis_path
+    else
+      flash[:error] = "There was an error deleting your Wiki. Please try again."
+      render :show
+    end
+  end
+
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :public)
+  end
+end
---
+++ test/controllers/wiki_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WikiControllerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
